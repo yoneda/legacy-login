c3a5e40b79f4b1d57ac2e2c493e833d10077b0b7
shou <nsshowdream@gmail.com>
2018-02-22 03:01:39 +0900

    add scrapbox api using request package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var request = require("request");
 var app = express();
 
 // 静的ファイル(image,css,javascript) をpublic フォルダに格納。
@@ -17,6 +18,13 @@ app.set("view engine","ejs");
 
 // トップ画面
 app.get("/",function(req,res){
+  request("https://scrapbox.io/api/pages/yoneda/?limit=3",function(error,response,body){
+    var json = JSON.parse(body);
+    var pages = json.pages;
+    for(var key in pages){
+      console.log(pages[key].title);
+    }
+  });
   res.render("index.ejs");
 });
 
