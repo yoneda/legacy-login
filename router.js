27e8b1d005fc034b48f8597bf2a06cdb15fcf9fe
yoneda <nsshowdream@gmail.com>
2020-04-12 21:33:07 +0900

    [fix] add github login test

--- router.js
+++ router.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
-const { auth, home, signup, login, setting, new:newHandler} = require("./handlers");
+const {
+  auth,
+  home,
+  signup,
+  login,
+  setting,
+  new: newHandler,
+} = require("./handlers");
 
 router.get("/", asyncHandler(auth), asyncHandler(home.get));
 router.get("/setting", asyncHandler(auth), asyncHandler(setting.get));
@@ -13,5 +20,32 @@ router.post("/logout", asyncHandler(setting.logout));
 router.post("/update", asyncHandler(setting.updatePass));
 router.get("/new", asyncHandler(auth), asyncHandler(newHandler.get));
 router.post("/new", asyncHandler(newHandler.post));
+
+const request = require("superagent");
+router.get(
+  "/github",
+  asyncHandler(async function (req, res, next) {
+    const { code } = req.query;
+    const url = "https://github.com/login/oauth/access_token";
+    const option = {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    };
+    const { access_token } = await request
+      .post(url)
+      .send(option)
+      .then((d) => d.body);
+    console.log(access_token);
+
+    const githubUrl = "https://api.github.com/user";
+    const { login } = await request
+      .get(githubUrl)
+      .set("User-Agent", "")
+      .set("Authorization", `token ${access_token}`)
+      .then((d) => d.body);
+    res.send(`your gh account is ${login}`);
+  })
+);
 
 module.exports = router;
